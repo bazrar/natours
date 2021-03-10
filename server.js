f82ec064c889d6b7f449d3aedd89819c6fd335b6
varbuzz <buzz.rejan@gmail.com>
2021-03-11 05:15:04 +0545

    connect db

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
-
+const DB = process.env.DATABASE_LOCAL;
+// console.log(process.env);
+mongoose
+  .connect(DB, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('connected to the db successfully');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 // console.log(process.env);
 
 const port = 3000;
