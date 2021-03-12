bf1da58cb897806fcbc30f6e25a6f7cbd2488e21
varbuzz <buzz.rejan@gmail.com>
2021-03-12 21:30:57 +0545

    model refactor

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,11 +2,57 @@ const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'a tour must have a name'],
+    required: [true, 'A Tour must have a name'],
     unique: true,
   },
-  rating: { type: Number, default: 4.5 },
-  price: { type: Number, required: [true, 'a tour must have a price'] },
+  duration: {
+    type: String,
+    required: [true, 'A Tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, ' A tour must have a difficulty'],
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A Tour must have a price'],
+  },
+  discount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imgCover: {
+    type: String,
+    required: [true, 'A Tour must have a cover image'],
+  },
+  images: {
+    type: [String],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDate: [Date],
 });
 
 // MODEL
