b0704f4e4dc00cd03a5d280682da7df81bfca841
varbuzz <buzz.rejan@gmail.com>
2021-03-15 05:53:36 +0545

    generate token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,37 @@
-const User = require("../models/userModel")
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 
-exports.signUp = async(req, res, next) => {
+exports.signUp = async (req, res, next) => {
+  try {
+    const { name, password, passwordConfirm, email } = req.body;
+    // var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
+    const newUser = await User.create({
+      name,
+      email,
+      password,
+      passwordConfirm,
+    });
+    var token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
 
-    try {
+    // console.log(newUser);
+    // console.log('################3');
+    // console.log(newUser.id);
+    // console.log(newUser._id);
 
-        const newUser = await User.create(req.body)
-    
-        res.status(201).json({
-            status: 'success', 
-            data: {
-                user: newUser
-            }
-        })
-    }catch (err) {
-        res.status(500).json({
-            status: 'fail', 
-            message: err
-        })
-    }
-}
+    // console.log(token);
+    res.status(201).json({
+      status: 'success',
+      token,
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
